8c9edc5cc7b147c55d5e4614e9f4543817b7e7ca
Peter C. Norton <spacey-github.com@ssr.com>
2022-06-20 01:58:47 -0400

    Better comments

--- build-services.sh
+++ build-services.sh
@@ -5,7 +5,9 @@ set -e -o pipefail
 # From https://stackoverflow.com/questions/49638532/docker-copy-file-to-host-from-within-dockerfile
 export DOCKER_BUILDKIT=1
 
+# Base image with packages installed 
 docker build -t courier-base -f Dockerfile-base  .
-
+# MTA image, for accepting mail delivery from the internet
 docker build -t courier-mta.service -f Dockerfile-mta .
+# MSA image, for allowing authenticated users to submit email delivery to other mail servers
 docker build -t courier-msa.service -f Dockerfile-msa .
