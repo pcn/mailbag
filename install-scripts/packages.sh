7c9eda5bf8380b1155d8b15e37ed2ea18aec876d
Peter C. Norton <1056756+pcn@users.noreply.github.com>
2024-08-02 03:25:46 -0400

    Changing out for mailcap

--- install-scripts/packages.sh
+++ install-scripts/packages.sh
@@ -5,12 +5,15 @@
 
 apt-get update
 
+# mime-support is removed in favor of directly using mailcap?
+# https://blueprints.launchpad.net/ubuntu/noble/i386/mime-support/3.66
+
 while ! echo y | apt-get install -y \
                          build-essential curl git gnupg  wget \
             software-properties-common rsync emacs-nox zip vim sudo \
             zlib1g-dev libelf-dev libffi-dev libsqlite3-dev \
             libbz2-dev libreadline-dev libssl-dev libedit-dev \
-            jq python3 bind9-host devscripts libldap2-dev mime-support \
+            jq python3 bind9-host devscripts libldap2-dev mailcap \
             expect libgdbm-dev libidn11-dev gnutls-dev gnutls-bin \
             libgcrypt-dev ghostscript mgetty-fax netpbm aspell \
             libpcre2-dev libaspell-dev libperl-dev debhelper locales; do   
